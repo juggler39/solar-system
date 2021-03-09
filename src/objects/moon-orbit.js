aa88a6e085c93a51aad824618d0ecc5eb7873670
Dmitriy Karpov <dmtkpv@gmail.com>
2021-03-09 02:26:34 +0200

    moon orbit

--- src/objects/moon-orbit.js
+++ src/objects/moon-orbit.js
@@ -1,4 +1,6 @@
-export default class MoonOrbitt {
+import Utils from '@/utils'
+
+export default class MoonOrbit {
 
 
 
@@ -9,73 +11,83 @@ export default class MoonOrbitt {
     constructor (options) {
 
 
-        // // options
-        //
-        // super(options);
-        // this.opacity = 0;
-        //
-        //
-        // // resize animation
-        //
-        // this.resize = gsap.to(this, {
-        //     size: this.system.activeOrbitSizes[this.index + 1],
-        //     duration: this.system.options.durations.translate,
-        //     paused: true,
-        //     ease: Power1.easeInOut,
-        //     onUpdate: () => {
-        //         this.setSize();
-        //         this.setTransform();
-        //     }
-        // })
-        //
-        //
-        // // spin animations
-        //
-        // this.spin = gsap.to(this, {
-        //     duration: 2 * Math.PI * this.origin.distance * this.system.options.speed,
-        //     angle: this.origin.angle + 360,
-        //     repeat: -1,
-        //     ease: Power0.easeNone,
-        //     onUpdate: () => {
-        //         if (this.opacity) this.setTransform();
-        //     }
-        // });
-        //
-        //
-        // // fade animation
-        //
-        // this.fade = gsap.to(this, {
-        //     opacity: 1,
-        //     duration: this.system.options.durations.fade,
-        //     paused: true,
-        //     ease: Power1.easeInOut,
-        //     onUpdate: () => {
-        //         this.setOpacity();
-        //     }
-        // })
-        //
-        //
-        // // event listeners
-        //
-        // this.system.on('pause', planet => {
-        //     this.spin.pause();
-        //     this.setTransform();
-        // })
-        //
-        // this.system.on('enter', planet => {
-        //     if (planet.moonOrbit === this) this.fade.play();
-        // })
-        //
-        // this.system.on('leave', planet => {
-        //     if (planet.moonOrbit === this) this.fade.reverse();
-        // })
-        //
-        //
-        // // render
-        //
-        // this.setOpacity();
+        // options
+
+        this.size = options.system.normalOrbitSizes.moon;
+        this.planet = options.planet;
+        this.system = options.system;
+        this.opacity = 0;
+        this.angle = this.planet.angle;
 
 
+        // create node
+
+        this.$node = Utils.createNode(`ps-orbit ps-orbit-${this.planet.orbit.index} ps-planet-${this.planet.index} ps-moon-orbit`, this.size);
+        this.system.$orbits.appendChild(this.$node);
+
+
+        // fade animation
+
+        this.fade = gsap.to(this, {
+            duration: this.system.options.durations.fade,
+            opacity: 1,
+            paused: true,
+            ease: Power1.easeInOut,
+            onUpdate: () => {
+                this.setTransform();
+                this.setOpacity();
+            }
+        })
+
+
+        // event listeners
+
+        this.system.on('enter', planet => {
+            if (planet.moonOrbit !== this) return;
+            this.setSize();
+            this.setTransform();
+            this.fade.play();
+        })
+
+        this.system.on('leave', planet => {
+            if (planet.moonOrbit !== this) return;
+            this.fade.reverse();
+        })
+
+        this.system.on('activate', planet => {
+            if (planet.moonOrbit === this) this.resize(this.system.activeOrbitSizes[0]);
+            else {
+                this.resize(this.system.activeOrbitSizes.moon);
+                this.fade.reverse();
+            }
+        })
+
+        this.system.on('deactivate', () => {
+            this.resize(this.system.normalOrbitSizes.moon);
+            this.fade.reverse();
+        })
+
+        this.setOpacity();
+
+    }
+
+
+
+    // ----------------------
+    // Helpers
+    // ----------------------
+
+    resize (size) {
+        this._resize && this._resize.kill();
+        this._resize = gsap.to(this, {
+            duration: this.system.options.durations.translate,
+            size,
+            onUpdate: () => {
+                if (!this.opacity) return;
+                this.setSize();
+                this.setTransform();
+            }
+        });
     }
 
 
@@ -86,6 +98,14 @@ export default class MoonOrbitt {
 
     setOpacity () {
         this.$node.style.opacity = this.opacity;
+    }
+
+    setSize () {
+        this.$node.style.width = this.$node.style.height = this.size + 'px';
+    }
+
+    setTransform () {
+        this.$node.style.transform = `translate(${this.planet.x - this.size / 2}px, ${this.planet.y - this.size / 2}px)`
     }
 
 }
